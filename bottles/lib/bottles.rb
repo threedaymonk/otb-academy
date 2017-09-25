88fbe17b17f57d5da3e3dbe030a534b20df40206
Adam Shafe <adam.shafe@onthebeach.co.uk>
2017-09-25 10:27:08 +0100

    Fixed the case of n=1

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -1,7 +1,11 @@
 class Bottles
 
   def verse(n)
+    if n > 1
      "#{n} bottles of beer on the wall, #{n} bottles of beer.\nTake one down and pass it around, #{n-1} bottles of beer on the wall.\n"
+   elsif n == 1
+     "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
+   end
   end
 
 end
