5b2b0c6cde8714257ef09d542c27b4a0f8d80879
Dan Brooks <dan.brooks@onthebeach.co.uk>
2017-09-25 11:01:05 +0100

    refactoring to handle plurality in bottle(s)

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -1,15 +1,20 @@
 class Bottles
+
   def verse(n)
-    if n == 8
-      "8 bottles of beer on the wall, 8 bottles of beer.\nTake one down and pass it around, 7 bottles of beer on the wall.\n"
-    elsif n == 3
-      "3 bottles of beer on the wall, 3 bottles of beer.\nTake one down and pass it around, 2 bottles of beer on the wall.\n"
-    elsif n == 1
-      "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
-    elsif n == 2
-      "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n"
-    elsif n == 0
-      "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
+    if n > 0
+      "#{prefix(n)}\n#{suffix(n)}\n"
+    end
+  end
+
+  def prefix(n)
+     "#{numberBottles(n)} of beer on the wall, #{numberBottles(n)} of beer."
+  end
+
+  def suffix(n)
+    if n == 1
+      "Take it down and pass it around, no more bottles of beer on the wall."
+    else
+      "Take one down and pass it around, #{numberBottles(n - 1)} of beer on the wall."
     end
   end
 
@@ -18,6 +23,14 @@ class Bottles
 
     if start == 8 && finish == 6
      "8 bottles of beer on the wall, 8 bottles of beer.\nTake one down and pass it around, 7 bottles of beer on the wall.\n\n7 bottles of beer on the wall, 7 bottles of beer.\nTake one down and pass it around, 6 bottles of beer on the wall.\n\n6 bottles of beer on the wall, 6 bottles of beer.\nTake one down and pass it around, 5 bottles of beer on the wall.\n\n"
+    end
+  end
+
+  def numberBottles(n)
+    if n == 1
+      "#{n} bottle"
+    else
+      "#{n} bottles"
     end
   end
 end
