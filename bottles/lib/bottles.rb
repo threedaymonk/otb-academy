f0c39177e3a2fe279892c35842b77daa4cbc8571
Dan Brooks <dan.brooks@onthebeach.co.uk>
2017-09-25 10:20:16 +0100

    Fix fifth spec

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -8,6 +8,8 @@ class Bottles
       "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
     elsif n == 2
       "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n"
+    elsif n == 0
+      "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
     end
   end
 end
