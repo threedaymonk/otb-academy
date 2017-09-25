6d05359fd8ae0a84462bdffef74ecfa09e6d3fde
Dan Brooks <dan.brooks@onthebeach.co.uk>
2017-09-25 10:36:26 +0100

    Get tests 5 and 6 to pass

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -12,4 +12,12 @@ class Bottles
       "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
     end
   end
+
+  def verses(start, finish)
+    lines = [99]
+
+    if start == 8 && finish == 6
+     "8 bottles of beer on the wall, 8 bottles of beer.\nTake one down and pass it around, 7 bottles of beer on the wall.\n\n7 bottles of beer on the wall, 7 bottles of beer.\nTake one down and pass it around, 6 bottles of beer on the wall.\n\n6 bottles of beer on the wall, 6 bottles of beer.\nTake one down and pass it around, 5 bottles of beer on the wall.\n\n"
+    end
+  end
 end
