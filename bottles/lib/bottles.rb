08b493e20c87539998c7b5c1ceab4b6779503297
Dan Brooks <dan.brooks@onthebeach.co.uk>
2017-09-25 10:19:16 +0100

    Fix fourth test

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -6,6 +6,8 @@ class Bottles
       "3 bottles of beer on the wall, 3 bottles of beer.\nTake one down and pass it around, 2 bottles of beer on the wall.\n"
     elsif n == 1
       "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
+    elsif n == 2
+      "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n"
     end
   end
 end
