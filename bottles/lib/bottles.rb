49607b1ccdc0c20e20156fb55e0df0c6e3dbbbf9
Adam Shafe <adam.shafe@onthebeach.co.uk>
2017-09-25 10:15:46 +0100

    Fixed the verse error

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -1,2 +1,7 @@
 class Bottles
+
+  def verse(n)
+     "#{n} bottles of beer on the wall, #{n} bottles of beer.\nTake one down and pass it around, #{n-1} bottles of beer on the wall.\n"
+  end
+
 end
