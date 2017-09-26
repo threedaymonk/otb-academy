da16230d2b5b7c6146753aa7b741effc1e77b0cd
Iain Kinnish <iainkinnish@Iains-MacBook-Pro.local>
2017-09-26 16:48:55 +0100

    Scoring Project complete.

--- the-language/spec/scoring_project_spec.rb
+++ the-language/spec/scoring_project_spec.rb
@@ -1,3 +1,86 @@
+SCORING_HASH = {1 => 100, 5 => 50, 2 => 0, 3 => 0,
+               4 => 0, 6 => 0}
+
+def score(dice)
+  return 0 if dice.empty?
+  arr = []
+  dice.each do |die|
+    arr << SCORING_HASH[die]
+  end
+  arr.inject(:+) + bonus_score(dice)
+end
+
+def bonus_score(dice)
+ triple = dice.select { |die| dice.count(die) > 2 }
+ if triple[0] == 1
+   700
+ elsif triple[0] == 5
+   350
+ elsif triple == []
+   0
+ else
+   triple[0] * 100
+ end
+end
+
+RSpec.describe "scoring a game of greed" do
+
+  it "scores an empty list as 0" do
+    expect( score([]) ).to eq( 0 )
+  end
+
+  it "scores a single 5 as 50" do
+
+    expect( score([5]) ).to eq( 50 )
+  end
+
+  it "scores a single 1 as 100" do
+
+    expect( score([1]) ).to eq( 100 )
+  end
+
+  it "scores multiple 1s and 5s as a sum of the individual scores" do
+
+    expect( score([1,5,5,1]) ).to eq( 300 )
+  end
+
+  it "scores 2s, 3s, 4s, and 6s as 0" do
+    expect( score([2,3,4,6]) ).to eq( 0 )
+  end
+
+  it "scores triple 1 as 1000" do
+    expect( score([1,1,1]) ).to eq( 1000 )
+  end
+
+  it "scores other triples as face value * 100" do
+    expect( score([2,2,2]) ).to eq( 200 )
+    expect( score([3,3,3]) ).to eq( 300 )
+    expect( score([4,4,4]) ).to eq( 400 )
+    expect( score([5,5,5]) ).to eq( 500 )
+    expect( score([6,6,6]) ).to eq( 600 )
+  end
+
+  it "can score mixed throws" do
+    expect( score([2,5,2,2,3]) ).to eq( 250 )
+    expect( score([5,5,5,5]) ).to eq( 550 )
+    expect( score([1,1,1,1]) ).to eq( 1100 )
+    expect( score([1,1,1,1,1]) ).to eq( 1200 )
+    expect( score([1,1,1,5,1]) ).to eq( 1150 )
+  end
+
+end
+
+# def triple_bonus(dice)
+#   triple = dice.select { |die| dice.count(die) > 2 }
+#   if triple[0] == 1
+#     1000
+#   elsif triple == []
+#     0
+#   else
+#     triple[0] * 100
+#   end
+# end
+
 # Greed is a dice game where you roll up to five dice to accumulate
 # points.  The following "score" function will be used to calculate the
 # score of a single roll of the dice.
@@ -26,68 +109,3 @@
 # More scoring examples are given in the tests below:
 #
 # Your goal is to write the score method.
-
-def score(dice)
-  arr = []
-  dice.each do |num|
-    arr << num * 100 if num == 1
-    arr << num * 10 if num == 5
-  end
-  arr.sum
-end
-
-# scores = dice.group_by do |n|
-#    return 100 if n == 1
-#   return 50 if n == 5
-# end
-# return scores
-
-RSpec.describe "scoring a game of greed" do
-
-  it "scores an empty list as 0" do
-    expect( score([]) ).to eq( 0 )
-  end
-
-  it "scores a single 5 as 50" do
-
-    expect( score([5]) ).to eq( 50 )
-  end
-
-  it "scores a single 1 as 100" do
-
-    expect( score([1]) ).to eq( 100 )
-  end
-
-  it "scores multiple 1s and 5s as a sum of the individual scores" do
-
-    expect( score([1,5,5,1]) ).to eq( 300 )
-  end
-
-  it "scores 2s, 3s, 4s, and 6s as 0" do
-    expect( score([2,3,4,6]) ).to eq( 0 )
-  end
-
-  it "scores triple 1 as 1000" do
-    skip
-    expect( score([1,1,1]) ).to eq( 1000 )
-  end
-
-  it "scores other triples as face value * 100" do
-    skip
-    expect( score([2,2,2]) ).to eq( 200 )
-    expect( score([3,3,3]) ).to eq( 300 )
-    expect( score([4,4,4]) ).to eq( 400 )
-    expect( score([5,5,5]) ).to eq( 500 )
-    expect( score([6,6,6]) ).to eq( 600 )
-  end
-
-  it "can score mixed throws" do
-    skip
-    expect( score([2,5,2,2,3]) ).to eq( 250 )
-    expect( score([5,5,5,5]) ).to eq( 550 )
-    expect( score([1,1,1,1]) ).to eq( 1100 )
-    expect( score([1,1,1,1,1]) ).to eq( 1200 )
-    expect( score([1,1,1,5,1]) ).to eq( 1150 )
-  end
-
-end
