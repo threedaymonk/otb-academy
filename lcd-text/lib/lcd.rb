e6ae88d6e353ab82c472961d6cce6767a2393d24
Iain Kinnish <iainkinnish@Iains-MacBook-Pro.local>
2017-09-27 15:02:22 +0100

    Completed LCD exercise.

--- lcd-text/lib/lcd.rb
+++ lcd-text/lib/lcd.rb
@@ -1,70 +1,54 @@
 class LCD
 
   NUM_TO_LCD = {
-    0 => (
-<<~SQUIGGLY_HEREDOC
- _
-| |
-|_|
-SQUIGGLY_HEREDOC
-    ), 1 => (
-<<~SQUIGGLY_HEREDOC
-
-|
-|
-SQUIGGLY_HEREDOC
-    ), 2 => (
-<<~SQUIGGLY_HEREDOC
- _
- _|
-|_
-SQUIGGLY_HEREDOC
-    ), 3 => (
-<<~SQUIGGLY_HEREDOC
-_
-_|
-_|
-SQUIGGLY_HEREDOC
-    ), 4 => (
-<<~SQUIGGLY_HEREDOC
-
-|_|
-  |
-SQUIGGLY_HEREDOC
-    ), 5 => (
-<<~SQUIGGLY_HEREDOC
- _
-|_
- _|
-SQUIGGLY_HEREDOC
-    ), 6 => (
-<<~SQUIGGLY_HEREDOC
- _
-|_
-|_|
-SQUIGGLY_HEREDOC
-    ), 7 => (
-<<~SQUIGGLY_HEREDOC
- _
-  |
-  |
-SQUIGGLY_HEREDOC
-    ), 8 => (
-<<~SQUIGGLY_HEREDOC
- _
-|_|
-|_|
-SQUIGGLY_HEREDOC
-    ), 9 => (
-<<~SQUIGGLY_HEREDOC
- _
-|_|
- _|
-SQUIGGLY_HEREDOC
-    )
+    0 => ([
+      ' _ ',
+      '| |',
+      '|_|',
+    ]), 1 => ([
+      '   ',
+      '  |',
+      '  |',
+    ]), 2 => ([
+      ' _ ',
+      ' _|',
+      '|_ ',
+    ]), 3 => ([
+      ' _ ',
+      ' _|',
+      ' _|',
+    ]), 4 => ([
+      '   ',
+      '|_|',
+      '  |',
+    ]), 5 => ([
+      ' _ ',
+      '|_ ',
+      ' _|',
+    ]), 6 => ([
+      ' _ ',
+      '|_ ',
+      '|_|'
+    ]), 7 => ([
+      ' _ ',
+      '  |',
+      '  |',
+    ]), 8 => ([
+      ' _ ',
+      '|_|',
+      '|_|',
+    ]), 9 => ([
+      ' _ ',
+      '|_|',
+      ' _|',
+    ])
   }
 
   def convert(num)
-    NUM_TO_LCD[num]
+    #num.to_s.split("").map { |num| NUM_TO_LCD[num.to_i] }
+    split_nums = num.split("").map { |num| num.to_i }
+    (0..2).map do |i|
+      split_nums.map { |num| NUM_TO_LCD[num][i] }.join
+    end.join("\n")
   end
 end
