5f8d6fa15dea244f30b1c05f0dffb2fecf5c42c8
Iain Kinnish <iainkinnish@Iains-MacBook-Pro.local>
2017-09-27 10:40:46 +0100

    Tweaks + implement discount calculation

--- harry_potter/lib/harry_potter.rb
+++ harry_potter/lib/harry_potter.rb
@@ -1,6 +1,17 @@
-class HarryPotter
+class Shop
 
-  def buy(first, second)
-    8 * (first + second)
+  DISCOUNT_HASH = { 2 => 5, 3 => 10, 4 => 20, 5 => 25 }
+
+  def initialize
+    @basket = []
+  end
+
+  def buy(books)
+    @basket = books
+    8 * (@basket.sum)
+  end
+
+  def discount(number)
+    DISCOUNT_HASH[number]
   end
 end
--- harry_potter/spec/harry_potter_spec.rb
+++ harry_potter/spec/harry_potter_spec.rb
@@ -1,25 +1,34 @@
 require 'harry_potter'
 
-describe HarryPotter do
+describe Shop do
 
   describe 'buy(first, second)' do
 
     context 'one book' do
 
       it 'allows you to purchase a book' do
-        expect(subject.buy(1, 0)).to eq 8
+        expect(subject.buy([1, 0])).to eq 8
       end
 
       it 'allows you to purchase multiple copies of the same book' do
-        expect(subject.buy(4, 0)).to eq 32
+        expect(subject.buy([4, 0])).to eq 32
       end
     end
 
     context 'two books' do
 
       it 'allows you to purchase a second book' do
-        expect(subject.buy(1, 2)).to eq 24
+        expect(subject.buy([1, 2])).to eq 24
       end
+    end
+  end
+
+  describe 'discount' do
+    it 'allows you to get a discount for your books' do
+      expect(subject.discount(2)).to eq(5)
+      expect(subject.discount(3)).to eq(10)
+      expect(subject.discount(4)).to eq(20)
+      expect(subject.discount(5)).to eq(25)
     end
   end
 end
